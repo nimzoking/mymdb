ef4ee7c5ffc445c1978792394c372812300bfa47
Carl Tillotson <nimzoking@gmail.com>
2019-02-23 20:28:21 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# My Movies Database
+
+Application based on code snippets from 'Building Django 2.0 Web Applications'
+
+# To Do
+
+Start to beef up the Bootstrap and add in the missing URLs to the nav bar
+
+Need to review Docker usage, does not work very well in Windows. 
---
+++ django/config/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import (static, )
+from django.urls import path, include
+from django.contrib import admin
+
+import core.urls
+import user.urls
+
+MEDIA_FILE_PATHS = static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('user/', include(('user.urls', 'user'), namespace='user')),
+    path('', include(('core.urls', 'core'),namespace='core')),
+] + MEDIA_FILE_PATHS
